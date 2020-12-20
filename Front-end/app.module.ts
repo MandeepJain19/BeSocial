6a313c85c39843d8cecf716f93b59a0408af66f0
MandeepJain19 <54706773+MandeepJain19@users.noreply.github.com>
2020-12-20 14:40:01 +0530

    Add files via upload

---
+++ Front-end/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+import { LoginComponent } from './Authentication/login/login.component';
+import { SignupComponent } from './Authentication/signup/signup.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    ComponentsModule,
+    ExamplesModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front-end/app.routing.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { LoginComponent } from './Authentication/login/login.component';
+import { SignupComponent } from './Authentication/signup/signup.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',component: ComponentsComponent },
+    { path: 'login',component: LoginComponent },
+    { path: 'signup',component: SignupComponent },
+    { path: 'icon',component: NucleoiconsComponent },
+    
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
